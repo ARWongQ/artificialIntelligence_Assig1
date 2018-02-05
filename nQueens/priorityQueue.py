49e6966cd7f20d90f6df9e2e6cab3a673e3b911d
Augusto Wong <augustowong2010@hotmail.com>
2018-02-04 23:27:22 -0500

    added prioriyQueue

---
+++ nQueens/priorityQueue.py
@@ -0,0 +1,21 @@
+import heapq
+
+#The class for the PriorityQueue (lowest in the top)
+class PriorityQueue:
+    def __init__ (self):
+        #Elements
+        self.queue = []
+        #Number of elements
+        self.index = 0
+
+    def push(self, Node, priority):
+        #Add element to the queue with its priority
+        heapq.heappush(self.queue,(priority,self.index,Node))
+        #Increase index
+        self.index += 1
+
+    def pop(self):
+        #decrease index
+        self.index -= 1
+        #Pop node in the top of queue
+        return heapq.heappop(self.queue)[-1]
