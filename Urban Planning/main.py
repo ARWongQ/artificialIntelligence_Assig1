1047073ab1e07c77b49df5513bb5fea9fe6737e6
kyleyoung14 <kcyoung@wpi.edu>
2018-02-06 15:05:02 -0500

    Small changes in main. Added sample texts

--- Urban Planning/main.py
+++ Urban Planning/main.py
@@ -1,2 +1,22 @@
-print("hello world from urban Planning")
-print("testing changes")
+import UPParser
+
+#Main Function
+def main():
+    print("Running Urban Planning")
+
+    # Receive input
+    searchType = input("Type of Search: 1 for greedy hill climbing and 2 for genetic: ")
+    fileName = input("File path for map: ")
+
+    # Load from file
+
+
+    #
+
+
+
+
+
+#Run the main function
+if __name__ == "__main__":
+    main()
