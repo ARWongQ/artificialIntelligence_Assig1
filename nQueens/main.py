216ca482a4b0ffb9a222ec49798e46692b701b36
Augusto Wong <augustowong2010@hotmail.com>
2018-02-06 20:21:40 -0500

    Fixed random queens to only take non repeated random rows

--- nQueens/main.py
+++ nQueens/main.py
@@ -8,16 +8,20 @@ def main():
     nValue = input("N value for the N-queens problem: ")
     searchType = input("Type of Search: 1 for A* and 2 for greedy hill climbing: ")
 
+    #Start the Board
     gameBoard = Board(nValue)
     print(" ")
 
+    #Set random Queens for the Board and print it to the user
     gameBoard.setRandomQueens()
     gameBoard.printBoard()
 
+    #Check how many queens are hitting each other
     gameBoard.checkTotalHittingQueens()
 
-    hitting = gameBoard.h
 
+    #Print the heurstic of the queens
+    hitting = gameBoard.h
     print("These many queens are hitting each other")
     print(hitting)
 
