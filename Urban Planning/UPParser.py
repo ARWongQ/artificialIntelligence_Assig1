ae76857086d8abaa09a3d6d8b0f68e17c7d328d6
kyleyoung14 <kcyoung@wpi.edu>
2018-02-07 20:11:36 -0500

    Worked on Urban Planning Hill Climb
    Worked on structure of urban planning solution and hill climb. Still having trouble with random placement at new iteration. The places are not being put in the array where I think I am telling them to go. Should be a simple change.

--- Urban Planning/UPParser.py
+++ Urban Planning/UPParser.py
@@ -1,9 +1,10 @@
 # noinspection SpellCheckingInspection,PyUnusedLocal,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon,PyTrailingSemicolon
+from UrbanMap import UrbanMap
 
 def printMapNicely(mapToPrint):
-    for row in range(len(mapToPrint)):
-        print(mapToPrint[row]);
-    print("");
+    for row in range(len(mapToPrint.map)):
+        print(mapToPrint.map[row])
+    print("")
 
 
 # noinspection PyRedundantParentheses
@@ -12,55 +13,56 @@ def parseUrbanMap(textfile):
     # 3 integers and a 3D array
 
     # Read in the file, pull apart the lines
-    f = open(textfile, 'r');
-    industrialCount = f.readline();
-    commercialCount = f.readline();
-    residentialCount = f.readline();
-    urbanMap = f.readlines();
-    f.close();
+    f = open(textfile, 'r')
+    industrialCount = f.readline()
+    commercialCount = f.readline()
+    residentialCount = f.readline()
+    urbanMap = f.readlines()
+    f.close()
 
-    print(industrialCount);
-    print(commercialCount);
-    print(residentialCount);
-    print(urbanMap);
+    print(industrialCount)
+    print(commercialCount)
+    print(residentialCount)
+    print(urbanMap)
 
     #This subfunction splits the urbanMap into an array
     # First, create the empty 3D array...
     #   Get the X-size from urbanMap[0]
 
-    mapLengthString = urbanMap[0];
+    mapLengthString = urbanMap[0]
     # split the string
-    mapLengthArray = mapLengthString.split(',');
+    mapLengthArray = mapLengthString.split(',')
     #we now have the dimensions required to make the array
 
     # noinspection PyUnusedLocal
-    urbanMapArray = [[["-" for dep in range(2)] for col in range(len(mapLengthArray))] for row in range(len(urbanMap))];
+    urbanMapArray = [[["-" for dep in range(2)] for col in range(len(mapLengthArray))] for row in range(len(urbanMap))]
     #now we have an empty array with dimensions CxRx2
-    print(urbanMapArray);
+    print(urbanMapArray)
 
     #Now we move data from the urbanMap array of strings into the 3darray
     for row in range(len(urbanMap)):
         #pull the data from urbanMap like we did above
-        mapString = urbanMap[row];
-        mapArray = mapString.split(",");
+        mapString = urbanMap[row]
+        mapArray = mapString.split(",")
         #print(mapArray);
         #move data from this array into the columns in the row
         for col in range(len(mapArray)):
-            print(row, col, 0);
-            currentElement = mapArray[col].rstrip();
-            print(currentElement);
-            urbanMapArray[row][col][0] = currentElement;
+            print(row, col, 0)
+            currentElement = mapArray[col].rstrip()
+            print(currentElement)
+            urbanMapArray[row][col][0] = currentElement
             if (currentElement == 'X' or currentElement == 'S'):
-                urbanMapArray[row][col][1] = currentElement;
+                urbanMapArray[row][col][1] = currentElement
 
     #Assuming this works, we now can print this
 
     #print(urbanMapArray);
     #Python's print statement is garbage, so here's a nicer custom one
-    printMapNicely(urbanMapArray);
+    newMap = UrbanMap(int(industrialCount), int(commercialCount), int(residentialCount), urbanMapArray)
+    printMapNicely(newMap)
     #Prints map in shape given by file, but each file element now also includes an empty space for the kind of building
     # that will go on top of it.
-    return industrialCount, commercialCount, residentialCount, urbanMapArray;
+    return newMap
 
 
 
--- Urban Planning/main.py
+++ Urban Planning/main.py
@@ -1,21 +1,33 @@
 import UPParser
+import UPGetScore
+from UrbanMap import UrbanMap
+import algorithms
 
 #Main Function
 def main():
     print("Running Urban Planning")
 
     # Receive input
-    searchType = input("Type of Search: 1 for greedy hill climbing and 2 for genetic: ")
-    fileName = input("File path for map: ")
+    searchType = raw_input("Type of Search: 1 for greedy hill climbing and 2 for genetic: ")
+    fileName = raw_input("File path for map: ")
 
     # Load from file
+    map = UPParser.parseUrbanMap(fileName)
 
+    # Print map info
+    UPParser.printMapNicely(map)
 
-    #
+    if(int(searchType) == 1): # if greedy hill climb
+        print("Beginning Greedy Hill Climb Algorithm")
+        bestScore, timeOfBest, finalMap = algorithms.hillClimb(map)
+    else: # else genetic
+        print("Beginning Genetic Algorithm")
+        algorithms.genetic(map)
 
-
-
-
+    print("The score for this map: ",bestScore)
+    print("Time of achieval: ", timeOfBest)
+    print("Final map: ")
+    UPParser.printMapNicely(finalMap)
 
 #Run the main function
 if __name__ == "__main__":
